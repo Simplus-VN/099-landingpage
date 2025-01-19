f73ca0408e98692212aeec6aa85e63211caecb58
Tran Xuan Son <tranxuanson98@gmail.com>
2025-01-19 18:48:33 +0700

    update footer and header

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,10 +1,15 @@
-import { Button } from "antd";
 import React from "react";
 
 const HeaderComponent: React.FC = () => {
   return (
-    <header>
-      <Button type="primary">Header</Button>
+    <header className="h-[88px] flex items-center bg-[rgba(255, 255, 255, 1)] max-lg:h-[72px]">
+      <div className="container mx-auto ">
+        <img
+          className="h-[48px] max-lg:h-[40px]"
+          src="/main-logo.png"
+          alt="main-logo"
+        />
+      </div>
     </header>
   );
 };
