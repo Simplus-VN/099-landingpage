76169d1d1a19105039cd87a942a9cf3bf33380ab
Tran Xuan Son <tranxuanson98@gmail.com>
2025-01-20 00:14:48 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,27 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#000000',
-        secondary: '#626D7C',
+        primary: "#000000",
+        secondary: "#626D7C",
       },
       borderRadius: {
-        'corner-03': '8px',
-        'corner-04': '12px',
+        "corner-03": "8px",
+        "corner-04": "12px",
       },
       backgroundColor: {
-        primary: '#FFC426'
-      }
+        primary: "#FFC426",
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "1312px",
+        xl: "1312px",
+        "2xl": "1312px",
+      },
     },
   },
   plugins: [],
