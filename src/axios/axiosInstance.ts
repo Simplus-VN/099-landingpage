b8d10a8497feaff3d7fd54e799fe5e5d3e8a5008
Rose <ducbui11a5@gmail.com>
2025-01-21 23:09:15 +0700

    update api url

--- src/axios/axiosInstance.ts
+++ src/axios/axiosInstance.ts
@@ -10,6 +10,7 @@ import { get, KEYS } from "../utils/localStorage";
 
 const axiosInstance: AxiosInstance = axios.create({
   // baseURL: 'http://localhost:3000',
+  baseURL: 'https://customer-api-dev.simplus.space',
   httpsAgent: false, // Disable HTTPS
   headers: {
     "Content-Type": "application/json",
